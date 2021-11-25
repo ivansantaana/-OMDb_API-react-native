1e5f2554a3a804837b8824f4e2edbefcb3c8c3cd
Ivan <ivansantanacalle@gmail.com>
2021-11-25 19:36:11 +0100

    Final commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React , { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Modal, ActivityIndicator, Image } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const App = () => {
   const [value, onChangeText] = useState('');
@@ -40,9 +41,46 @@ const App = () => {
           onPress={onPress}
         ><Text>Search</Text>
         </TouchableOpacity>
+        <Modal
+          style={styles.modalSettings}
+          animationType="fade"
+          transparent={false}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(false);
+          }}
+        >
+          {(!isLoading) ? 
+            <ScrollView>
+              <Image
+                source={{uri: searchResult.Poster}}
+                style={styles.backgroundImage}
+              />
+              <Text>Title: {searchResult.Title}</Text>
+              <Text>Type: {searchResult.Type}</Text>
+              <Text>Genre: {searchResult.Genre}</Text>
+              <Text>Actors: {searchResult.Actors}</Text>
+              <Text>Awards: {searchResult.Awards}</Text>
+              <Text>Director: {searchResult.Director}</Text>
+              <Text>Languages: {searchResult.Language}</Text>
+              <Text>Sinopsis: {searchResult.Plot}</Text>
+              <Text>Released: {searchResult.Released}</Text>
+              <Text>Runtime: {searchResult.Runtime}</Text>
+              <Text>Writer: {searchResult.Writer}</Text>
+              <Text>imdbRating: {searchResult.imdbRating}</Text>
+              <Text>imdbVotes: {searchResult.imdbVotes}</Text>
+              <Text>Seasons: {searchResult.totalSeasons}</Text>
+            </ScrollView>
+            :
+            <View style={[styles.container]}>
+              <ActivityIndicator size={100} color="#4CB53D" />
+            </View>
+          }
+        </Modal>
     </View> 
   );
 };
+
 export default App;
 
 
@@ -72,5 +110,14 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     alignSelf: 'stretch'
-    }
+  },
+  modalSettings: {
+    justifyContent : 'center',
+    alignItems: 'center'
+  },
+  backgroundImage: {
+    resizeMode: 'contain',
+    flex: 1,
+    aspectRatio: 1
+  }
 });
